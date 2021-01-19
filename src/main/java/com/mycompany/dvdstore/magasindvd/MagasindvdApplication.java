d721683260000acac437deab5d7cad4652ee3799
Baz <slampas@hotmail.fr>
2021-01-19 12:36:29 +0100

    TP1 DONE

--- src/main/java/com/mycompany/dvdstore/magasindvd/MagasindvdApplication.java
+++ src/main/java/com/mycompany/dvdstore/magasindvd/MagasindvdApplication.java
@@ -1,5 +1,10 @@
 package com.mycompany.dvdstore.magasindvd;
 
+import java.util.Scanner;
+
+import com.mycompany.dvdstore.magasindvd.entity.Movie;
+import com.mycompany.dvdstore.magasindvd.service.MovieService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,30 @@ public class MagasindvdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MagasindvdApplication.class, args);
+
+		
+		Movie movie = new Movie();
+		String genre;
+		String titre;
+		
+
+		System.out.println("--------------------------");
+		System.out.println("sasir le titre du film à ajouter ");
+		System.out.println("--------------------------");
+		Scanner sc = new Scanner(System.in);
+		titre = sc.nextLine();
+		movie.setTitle(titre);
+		System.out.println("--------------------------");
+		System.out.println("sasir le genre du film à ajouter ");
+		System.out.println("--------------------------");
+		genre = sc.nextLine();
+		movie.setGenre(genre);
+		sc.close();
+
+		MovieService movieService = new MovieService();
+		movieService.registerMovie(movie);
+
+
 	}
 
 }
--- src/main/java/com/mycompany/dvdstore/magasindvd/service/MovieService.java
+++ src/main/java/com/mycompany/dvdstore/magasindvd/service/MovieService.java
@@ -1,15 +1,23 @@
 package com.mycompany.dvdstore.magasindvd.service;
 
 import com.mycompany.dvdstore.magasindvd.entity.Movie;
+import com.mycompany.dvdstore.magasindvd.repository.MovieRepository;
 
 public class MovieService {
 
+    private MovieRepository movieRepository = new MovieRepository();
+    
+    
+    public void registerMovie(Movie e){
+        movieRepository.add(e);
 
-    public void registerMovie(Movie movie){
-
-        
+    
 
         
     }
+
+
+
+
     
 }
